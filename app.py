2eb78c96be7932ded03ae0d9700c8ef68273febc
diamondhelm <S577828@nwmissouri.edu>
2024-11-07 00:40:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import plotly.express as px
 import palmerpenguins
 import seaborn as sns
-from shiny.express import input, ui, render
+from shiny.express import input, ui
+from shiny import render, reactive
 from shinywidgets import render_plotly
 from palmerpenguins import load_penguins
 
-penguins= load_penguins()
+penguins_df = palmerpenguins.load_penguins()
 
 ui.page_opts(title="Diamond's Penguin Figures", fillable=True)
 
@@ -38,29 +39,42 @@ with ui.layout_columns():
         ui.card_header("Plotly of Penguin Species")
         @render.data_frame
         def table():
-            return render.DataTable(data=penguins)
+            return render.DataTable(data=penguins_df)
 
     with ui.card(full_screen=True):
         ui.card_header( "Grid of Penguin Species")
         @render.data_frame
         def grid():
-            return render.DataGrid(data=penguins)
+            return render.DataGrid(data=penguins_df)
 
     with ui.layout_columns():
         with ui.card(full_screen=True):
             ui.card_header("Plotly Histogram:Penguins by Body Mass")
         @render_plotly
         def plot1():
-            return px.histogram(penguins, x="body_mass_g", color="species", nbins=input.plotly_bin_count())
+            return px.histogram(penguins_df, x="body_mass_g", color="species", nbins=input.plotly_bin_count())
             
     with ui.card(full_screen=True):
         ui.card_header("Penguins by Flipper Length")
         @render.plot
         def plot2():
-            return sns.histplot(data=penguins, x="flipper_length_mm", hue="species", bins=input.seaborn_bin_count())
+            return sns.histplot(data=penguins_df, x="flipper_length_mm", hue="species", bins=input.seaborn_bin_count())
 
 with ui.card(full_screen=True):
     ui.card_header("Plotly Scatterplot: Various Species")
     @render_plotly
     def plotly_scatterplot():
-        return px.scatter(data_frame=penguins, x="body_mass_g", y="bill_length_mm", color="species", hover_name="island", symbol="sex")
+        return px.scatter(data_frame=penguins_df, x="body_mass_g", y="bill_length_mm", color="species", hover_name="island", symbol="sex")
+
+# --------------------------------------------------------
+# Reactive calculations and effects
+# --------------------------------------------------------
+
+# Add a reactive calculation to filter the data
+# By decorating the function with @reactive, we can use the function to filter the data
+# The function will be called whenever an input functions used to generate that output changes.
+# Any output that depends on the reactive function (e.g., filtered_data()) will be updated when the data changes.
+
+@reactive.calc
+def filtered_data():
+    return penguins_df
